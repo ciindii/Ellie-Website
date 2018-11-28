d43688f3b81ec7344d1107ff6598f10b1a0dcb6d
ciindii <cindythitruong@gmail.com>
2018-11-28 14:18:42 -0800

    closures

--- scripts.js
+++ scripts.js
@@ -40,3 +40,20 @@ $(function () {
     }, 300);
   });
 });
+
+
+function buildFuntion() {
+  var arr = [];
+  for (let i = 0; i < 3; i++) {
+    arr.push(function (j) {
+      return function () {
+        console.log(j);
+      };
+    }(i));
+  } return arr;
+}
+
+var fs = buildFunction();
+fs[0]();
+fs[1]();
+fs[2]();
