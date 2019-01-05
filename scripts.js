f826ca7102b8a2efd81f9a03ad7a9070fdcf2bb3
ciindii <cindythitruong@gmail.com>
2019-01-04 16:20:29 -0800

    fixed filter image js

--- scripts.js
+++ scripts.js
@@ -27,3 +27,17 @@ window.onclick = function (event) {
     }
   }
 };
+
+//filter images in gallery
+$(function () {
+  var selectedClass = '';
+  $('.filter').click(function () {
+    selectedClass = $(this).attr('data-rel');
+    $('#gallery').fadeTo(100, 0.1);
+    $('#gallery div').not('.' + selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function () {
+      $('.' + selectedClass).fadeIn().addClass('animation');
+      $('#gallery').fadeTo(300, 1);
+    }, 300);
+  });
+});
